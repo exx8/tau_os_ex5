e1c47c5cfc1498eefcad3cbbc6be4166f09f9e7c
exx8 <exx8eran@gmail.com>
2021-01-13 23:46:33 +0200

    .

--- pcc_server.c
+++ pcc_server.c
@@ -131,14 +131,14 @@ int main(int argc, char **argv) {
         readData(&length, s, sizeof(length));
         char string2process[length];
         readData(&string2process, s, sizeof(char) * length);
-        char * currentString=string2process;
+        char * currentCharAddress=string2process;
         while (!sickConnection && length > 0) {
-            if (isPrintable(*currentString)) {
+            if (isPrintable(*currentCharAddress)) {
                 numOfPrintable++;
-                pcc_total[*currentString]++;
+                pcc_total[*currentCharAddress]++;
             }
             length--;
-            currentString++;
+            currentCharAddress++;
         }
         sendData(&numOfPrintable, s, sizeof numOfPrintable);
     }
