ba8736b2b1b848eb696cd184a768009420f378d5
exx8 <exx8eran@gmail.com>
2021-01-14 14:51:19 +0200

    .

--- pcc_server.c
+++ pcc_server.c
@@ -82,6 +82,7 @@ void readData(void *data_buf, int confd, int notRead) {
     int totalsent = 0;
     // keep looping until nothing left to write
     while (notRead > 0) {
+        printf("in read, %d left",notRead);
         // notRead = how much we have left to write
         // totalsent  = how much we've written so far
         // nsent = how much we've written in last write() call */
@@ -113,23 +114,23 @@ int main(int argc, char **argv) {
     // INADDR_ANY = any local machine address
     sin2.sin_addr.s_addr = htons(INADDR_ANY);
     sin2.sin_port = (port);
-    int s = create_socket(&sin2, port);
+    int acceptSocket = create_socket(&sin2, port);
 
-    err_handler(bind(s, (struct sockaddr *) &sin2, sizeof(sin2)));
-    err_handler(listen(s, 10));
+    err_handler(bind(acceptSocket, (struct sockaddr *) &sin2, sizeof(sin2)));
+    err_handler(listen(acceptSocket, 10));
     unsigned int numOfPrintable = 0;
     struct sockaddr_in peerAddress;
     socklen_t len = (socklen_t) sizeof(peerAddress);
     while (shouldIContinue) {
         sickConnection = 0;// no problems so far with connection.
-        const int status = accept(s, (struct sockaddr *) &peerAddress, &len);
-        if (status == -1 && errno == EAGAIN)
+        const int readSocketOrStatus = accept(acceptSocket, (struct sockaddr *) &peerAddress, &len);
+        if (readSocketOrStatus == -1 && errno == EAGAIN)
             continue;
-        err_handler(status);
+        err_handler(readSocketOrStatus);
         unsigned int length;
-        readData(&length, s, sizeof(length));
+        readData(&length, readSocketOrStatus, sizeof(length));
         char string2process[length];
-        readData(&string2process, s, sizeof(char) * length);
+        readData(&string2process, readSocketOrStatus, sizeof(char) * length);
         char * currentCharAddress=string2process;
         while (!sickConnection && length > 0) {
             if (isPrintable(*currentCharAddress)) {
@@ -139,11 +140,11 @@ int main(int argc, char **argv) {
             length--;
             currentCharAddress++;
         }
-        sendData(&numOfPrintable, s, sizeof numOfPrintable);
+        sendData(&numOfPrintable, readSocketOrStatus, sizeof numOfPrintable);
     }
     for (int k = LOWER_LIMIT; k <= UPPER_LIMIT; k++) {
         printf("char '%c' : %u times\n", k, pcc_total[k]);
     }
-    close(s);
+    close(acceptSocket);
     return 0;
 }
